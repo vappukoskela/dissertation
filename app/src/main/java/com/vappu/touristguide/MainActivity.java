279fd48b12035ab84e447b3a11c4b0f28e0b5cea
psyvk <psyvk@nottingham.ac.uk>
2018-01-30 14:03:04 +0000

    Created Activities

---
+++ app/src/main/java/com/vappu/touristguide/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vappu.touristguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View view) {
+
+    }
+}
